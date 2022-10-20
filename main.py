6f7dca9da929d2a7a4c747d240baf2d8ff002cfd
Maria Valero <maria.valero@thoughtworks.com>
2022-10-20 20:26:18 +0200

    Add static backend route

--- main.py
+++ main.py
@@ -4,18 +4,36 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def frotend_index():
     return send_from_directory("./html/", "index.html")
 
 
 @app.route("/html/<string:name>")
-def html(name):
+def frotend_html(name):
     return send_from_directory("./html/", name)
 
 
 @app.route("/images/<string:name>")
-def images(name):
+def frotend_images(name):
     return send_from_directory("./html/images", name)
+
+
+@app.route("/backend/match/<string:name>/choices")
+def backend_match(name):
+    choices = {
+        "match_name": name,
+        "choices": [
+            {
+                "player": "Flors",
+                "choice": "water"
+            },
+            {
+                "player": "Maria",
+                "choice": "fire"
+            }
+        ]
+    }
+    return jsonify(choices)
 
 
 if __name__ == "__main__":
