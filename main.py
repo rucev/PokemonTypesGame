84fa8669002f071682026ce5600acf63176cbfc6
Maria Valero <maria.valero@thoughtworks.com>
2022-10-20 18:36:22 +0200

    Serve images

--- main.py
+++ main.py
@@ -3,24 +3,20 @@ from flask import Flask, send_from_directory, jsonify
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def index():
-    return send_from_directory("./html/","index.html")
+    return send_from_directory("./html/", "index.html")
 
-@app.route("/choices")
-def choices():
-    return send_from_directory("./html/","choices.html")
 
-@app.route("/match")
-def match():
-    return send_from_directory("./html/","match.html")
+@app.route("/html/<string:name>")
+def html(name):
+    return send_from_directory("./html/", name)
 
-@app.route("/wins")
-def wins():
-    return send_from_directory("./html/","wins.html")
+
+@app.route("/images/<string:name>")
+def images(name):
+    return send_from_directory("./html/images", name)
+
 
 if __name__ == "__main__":
     app.run()
-
-
