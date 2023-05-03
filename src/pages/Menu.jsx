513f7437e4c198f77c9a1dbec9a5f49f6e61952b
Flors Rueda <ruedaflors@gmail.com>
2023-05-03 18:48:28 +0200

    fix image vite bug

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -10,7 +10,7 @@ export default function Menu(props) {
     }
 
     return <section className="menu">
-        <img className="menu__title" src="../../public/title.png" alt="title" />
+        <img className="menu__title" src="https://raw.githubusercontent.com/rucev/PokemonTypesGame/567e8951099d7ef56029be23c296733fdc9df872/assets/images/title.webp" alt="title" />
         <div className="menu__buttons">
             <button className="menu__buttons--button" onClick={handleGameClick}><span>PLAY</span></button>
             <button className="menu__buttons--button" onClick={handleRulesClick}><span>RULES</span></button>
--- src/rules.js
+++ src/rules.js
@@ -2,26 +2,26 @@ export const SELECTIONS = [
   {
     name: 'fire',
     beats: ['plant', 'ice'],
-    src: '../public/fire.png',
+    src: 'https://raw.githubusercontent.com/rucev/PokemonTypesGame/567e8951099d7ef56029be23c296733fdc9df872/assets/images/fire.webp',
   },
   {
     name: 'water',
     beats: ['fire', 'rock'],
-    src: '../public/water.png',
+    src: 'https://raw.githubusercontent.com/rucev/PokemonTypesGame/567e8951099d7ef56029be23c296733fdc9df872/assets/images/water.webp',
   },
   {
     name: 'plant',
     beats: ['water', 'rock'],
-    src: '../public/plant.png',
+    src: 'https://raw.githubusercontent.com/rucev/PokemonTypesGame/567e8951099d7ef56029be23c296733fdc9df872/assets/images/plant.webp',
   },
   {
     name: 'ice',
     beats: ['water', 'plant'],
-    src: '../public/ice.png',
+    src: 'https://raw.githubusercontent.com/rucev/PokemonTypesGame/567e8951099d7ef56029be23c296733fdc9df872/assets/images/ice.webp',
   },
   {
     name: 'rock',
     beats: ['fire', 'ice'],
-    src: '../public/rock.png',
+    src: 'https://raw.githubusercontent.com/rucev/PokemonTypesGame/567e8951099d7ef56029be23c296733fdc9df872/assets/images/rock.webp',
   },
 ];
